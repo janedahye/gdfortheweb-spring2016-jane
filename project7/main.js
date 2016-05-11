e3312396ad85819bbc38126d73e155251cf12ddb
janedahye <jchang05@risd.edu>
2016-05-11 14:09:31 -0400

    flagged statements from nasa

--- project7/main.js
+++ project7/main.js
@@ -35,3 +35,9 @@ function Timer() {
 }
 
 
+  var overlay= document.getElementById('overlay');
+  var video= document.getElementById('v');
+  video.addEventListener('progress', function() {
+    var show= video.currentTime>=5 && video.currentTime<10;
+    overlay.style.visibility= show? 'visible' : 'visible';
+  }, false);
